5cfacb1d4ebafeb38780da9bd79d94d963295eb5
Stephanie Guevara <stephanie.estela.guevara.gullo@everis.com>
2019-04-01 13:52:02 +0200

    create components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './comoponents/home/home.component';
+import { SearchComponent } from './comoponents/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  {path: 'search', component: SearchComponent},
+  {path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './comoponents/home/home.component';
+import { SearchComponent } from './comoponents/search/search.component';
+import { ArtistComponent } from './comoponents/artist/artist.component';
+import { NavbarComponent } from './comoponents/shared/navbar/navbar.component';
+import { ROUTES } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
